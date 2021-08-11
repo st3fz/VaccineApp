41a89a6d4cd4ee629331bd5f1282d99ff209848f
Steffi <steffi__98@hotmail.com>
2021-08-11 21:25:07 +0800

    Added user api mock

---
+++ usersapi/src/test/java/com/steffi/usersapi/UsersapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.steffi.usersapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
