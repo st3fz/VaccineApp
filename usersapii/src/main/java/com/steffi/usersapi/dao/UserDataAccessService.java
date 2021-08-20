3a3f4ccc6997ec874620550c9a7cda51e94e74e3
Steffi <steffi__98@hotmail.com>
2021-08-20 19:00:03 +0800

    users api?

---
+++ usersapii/src/main/java/com/steffi/usersapi/dao/UserDataAccessService.java
@@ -0,0 +1,36 @@
+package com.steffi.usersapi.dao;
+
+import com.steffi.usersapi.model.User;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository("postgres")
+public class UserDataAccessService implements UserDao {
+    @Override
+    public int insertUser(UUID id, User user) {
+        return 0;
+    }
+
+    @Override
+    public List<User> selectAllUsers() {
+        return List.of(new User(UUID.randomUUID(), "FROM POSTGRES DB"));
+    }
+
+    @Override
+    public Optional<User> selectUserById(UUID id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public int deleteUserById(UUID id) {
+        return 0;
+    }
+
+    @Override
+    public int updateUserById(UUID id, User user) {
+        return 0;
+    }
+}
