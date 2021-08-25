b4b1b16e11fbcdf26ff2dafe08c2dc59a6235605
Steffi <steffi__98@hotmail.com>
2021-08-26 04:23:26 +0800

    🃏 Cards
    -Added folder for cards to store cards data in homepage
    -Removed cards dummy text and replaced with cards data

--- Web/src/pages/Home/Home.tsx
+++ Web/src/pages/Home/Home.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import './Home.scss';
-import { CardsData } from './CardsData';
+import { CardsData } from './Cards/CardsData';
 
 export default function Home() {
     return(
@@ -11,38 +11,20 @@ export default function Home() {
                 <h3 className="mt-1">Welcome to the vaccine app!</h3>
                 <h6  className="font-italic mt-3">In here you will find many useful features to help you cope with the covid apocalypse...I mean times!</h6>
             </div>
-            <div className="row">
-            <h5 className="mt-4">Check out some of the features we have below: </h5>
-                {CardsData.map((card, index)=>{
-                    return(
-                        <div className="card col-12">
-                            <div key={index} className="card-body mt-3">
-                                <h3 className="class-title">{card.title}</h3>
-                                <h4 className="class-subtitle">{card.subtitle}</h4>
-                                <p className="card-text">{card.text}</p>
-                                <button className="btn btn-secondary">{card.button}</button>
-                            </div>
+            <div className="container-fluid">
+                <div className="card-deck">
+            {CardsData.map((card, index)=>{
+                return(
+                    <div className="card">
+                        <div key={index} className="card-body mt-3">
+                            <h3 className="class-title">{card.title}</h3>
+                            <h4 className="class-subtitle">{card.subtitle}</h4>
+                            <p className="card-text">{card.text}</p>
+                            <button className="btn btn-secondary">{card.button}</button>
                         </div>
-                        )
-                })}
-
-                        <div className="card col-12">
-                            <div className="card-body mt-3">
-                                <h3 className="class-title">try</h3>
-                                <h4 className="class-subtitle">hm</h4>
-                                <p className="card-text">weird</p>
-                                <button className="btn btn-secondary">trirf</button>
-                            </div>
-                        </div>
-
-                        <div className="card col-3">
-                            <div className="card-body mt-3">
-                                <h3 className="class-title">try</h3>
-                                <h4 className="class-subtitle">hm</h4>
-                                <p className="card-text">weird</p>
-                                <button className="btn btn-secondary">trirf</button>
-                            </div>
-                        </div>
+                    </div>
+                    )
+                })}</div>
             </div>
         </div>
     )
