8f87aa39c7e27c97a9cc954758cfdd51d83a8c8c
Steffi <steffi__98@hotmail.com>
2021-08-30 01:43:39 +0800

    ✔ Added Backend and Frontend

---
+++ FrontEnd/Web_App/src/components/Sidebar/SidebarData.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import * as FaIcons from "react-icons/fa";
+import * as AiIcons from "react-icons/ai";
+import * as IoIcons from "react-icons/io";
+
+export const SidebarData = [
+    {
+        title: "Home",
+        path: "/",
+        icon: <AiIcons.AiFillHome />
+    },
+    {
+        title: "Log In",
+        path: "/auth",
+        icon: <IoIcons.IoIosPaper />
+    },
+    {
+        title: "Chat",
+        path: "/chat",
+        icon: <FaIcons.FaEnvelopeOpen />
+    }
+];
